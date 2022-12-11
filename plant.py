3008ca5e66c2a90e3ec13b8fac827dc0141a3577
Jake <jgjedi@gmail.com>
2022-12-11 03:31:18 -0700

    added a whole bunch of stuff, currently setup to test regional biases and several of the csvs are formatted wrong...

--- plant.py
+++ plant.py
@@ -1,9 +1,11 @@
 import pprint
+from bisect import bisect_left
 import os
 import math
 import uuid
 import random
 import regex as re
+import numpy as np
 import pandas as pd
 pd.set_option('display.max_rows', None)
 #All we want to do for the moment is make a class that looks up the "string" of the regional bias and loads the biases as a datafram to a dataframe object in an attribute. 
@@ -15,6 +17,7 @@ non-underscored methods and attributes are for setting the plant
 repeat=0
 class Plant:
 	def __init__(self,regional_bias):
+		#data is probably going to be assigned by inheritance from a game object
 		self._data = {"id":"UUID",
 		"date_created":"date_timestamp",
 		"version":"sample_version",
@@ -23,19 +26,18 @@ class Plant:
 		"price":0,
 		"price_string":"descriptive_price_string"}
 		self._plant_bias_obj = self._generate_bias_obj()
-		print()
 		self.plant=None
 		self.plant_name = None
 		self.regions = {1:'Centriss',2:'Tentacular',3:'Mormiria',4:'Tirelessnight',5:'Reyawinn',0:'Xilewood'}
-		self.regional_bias = regional_bias
-		self.region = "sample_region_name"
-		self._region_bias = regional_bias
+		self.regional_bias = (regional_bias,self.get_regional_bias_int(regional_bias))
+		self.all_plantnames_in_region=self._plant_bias_obj[self.regions[self.regional_bias[1]]]['_dataframe'].iloc[1,:][1:].tolist()
 		self._plant_id = "create a unique ID based upon a generated NAME for the plant so I can identify when stuff gets made"
 		self.potence = -1
+	def get_regional_bias_int(self,r):
+		return [i for i in self.regions if self.regions[i]==r][0]
 	def _sanitize_bias_keystring(self,filepath):
 		return re.search(r'.*\_',filepath).group(0)[7:-1].capitalize()
 	def _generate_bias_obj(self):
-		
 		obj={}
 		bias_dir=r'./bias/'
 		try:
@@ -47,16 +49,36 @@ class Plant:
 		except Exception as e:
 			raise ValueError(f'Could not load the biases files - Error:{e}')
 		return obj
+	def set_plant_attr(self,plant):
+		pass
+		#anytime the plant getters are run this is run
+		#set plant name attr
+		#set plant attr
+		#set plant columns while on the attr
+
 	def get_plant_power_profile(self):
 		pass
 	def get_plant_price(self):
 		pass
 	def get_plant(self,rgn,col):
-		return self._plant_bias_obj[self.regions[rgn]]['_dataframe'].iloc[:,[0,col]]
+		plant = self._plant_bias_obj[self.regions[rgn]]['_dataframe'].iloc[:,[0,col]]
+		self.plant=plant
+		return plant
+	def get_rgn_weighted_plant(self):
+		df=self._plant_bias_obj[self.regions[self.regional_bias[1]]]['_dataframe']
+		options=df.iloc[1].tolist()[1:] #MIGHT NEED TO TAKE THIS OUT
+		weights=np.array([i for i in df.iloc[0,:]][1:]).astype(float)
+		cdf=np.cumsum(weights/np.sum(weights))
+		roll=np.random.random()
+		idx=bisect_left(cdf, roll)
+		plant=df.iloc[:,idx]
+		self.plant_name=plant[1]
+		self.plant=plant
+		return plant
 	def get_random_plant(self,rgn):
 		col=random.randint(0,self._plant_bias_obj[self.regions[rgn]]['_dataframe'].shape[1]-1)
 		plant=self.get_plant(rgn,col)
-		# self.plant_name=plant['values'][2]
+		self.plant=plant
 		return plant
 	def set_plant_state(self,**kwargs):
 		#kwarg driven function that sets the state of the class to contain a specific PLANT!
@@ -86,18 +108,60 @@ class Plant:
 			raise NameError(f'Please enter a valid region: {self.regions}')
 	def compute_properties(self):
 		pass
-
 	def compute_rarity(self):
-		#there is a row that can be rowsummed to get the occurrence range for the region
-		span=0
+		#  https://stackoverflow.com/questions/57217923/what-are-pythonic-methods-for-implementing-random-roll-tables
+		#this method is going to make a huge list of tuples that will be massive - around a thousand elements per use. so it needs to be fast - maybe it needs to be a numpy array
+		check_bool=(self.plant is not None)
+		if check_bool:
+			df=self._plant_bias_obj[self.regions[self.regional_bias[1]]]['_dataframe']
+			occur_range=df.iloc[0][1:].astype(int).sum()
+			occur_val=self.plant['values'][0]
+			return float(occur_val)/occur_range
+			# occurence_prob=self.plant.loc['labels'self.plant_name=]
+
+
+
+
+			#here is a row that can be rowsummed to get the occurrence range for the region
+			# occur_span=df.iloc[0,:].sum(axis=1)
+			# print(occur_span)
+
 	
 
+# def count_elements(seq) -> dict:
+# """Tally elements from `seq`."""
+# ...     hist = {}
+# ...     for i in seq:
+# ...         hist[i] = hist.get(i, 0) + 1
+# ...     return hist
+
+def count_elements(seq):
+	hist={}
+	for i in seq:
+		hist[i] = hist.get(i,0)+1
+	return hist
 
 def checkit():
-	x = Plant('Cenriss')
-	for i in range(10):
-		x.get_plant_col(rgn=1,randomize=True)
-		print(f'Plant #{i}...is {x.plant_name}')
+	x = Plant('Centriss')
+	#test plant maker
+	# for i in range(10):
+	# 	x.get_plant_col(rgn=1,randomize=True)
+	# 	print(f'Plant #{i}...is {x.plant_name}')
+
+	#test rarity
+
+	x.get_plant_col(rgn=1,randomize=True)
+	res=[]
+	for i in range(1000):
+		x.get_rgn_weighted_plant()
+		res.append(x.plant_name)
+		# print('{}\r'.format(i),end='\r')
+	result_dic=count_elements(res)
+	_dic=((value,key) for (key,value) in result_dic.items())
+	s=sorted(_dic,reverse=True)
+	pprint.pprint(s)
+	# test the function sometime and make sure its a histogram
+
 
 if __name__ =="__main__":
 	checkit()
