614ff48a4d5147747517c48faec0b1a58194b14d
Jake <jgjedi@gmail.com>
2024-01-30 18:30:26 -0700

    Super Progress! PlantObject added AlchemyFactory now can get common effects and a bunch of progress implementing helper functions for calculating prefactors for duration and magnitude

--- docs/oblivion/oblivion_alchemy.py
+++ docs/oblivion/oblivion_alchemy.py
@@ -1,33 +1,22 @@
 import pandas as pd
 import pprint
+from plant import PlantFactory
 pd.set_option('display.max_rows',500)
 
-def construct_all_ingredients_df(path_to_ingredients):
-	ing_df = pd.read_csv(path_to_ingredients)
+def construct_df(path_to_csv):
+	ing_df = pd.read_csv(path_to_csv)
+	#process all casing to snake
 	for col in ing_df.columns:
 		ing_df[col] = ing_df[col].apply(lambda x: str(x).lower().replace(" ", "_"))
-	print(ing_df)
 	return ing_df
 
 
-
-
-# I NEED TO RETHINK HOW I AM HANDLING the instrument strength there needs to be a function that maps "strength"
-
-# Quality	Strength
-# Novice	0.1
-# Apprentice	0.25
-# Journeyman	0.5
-# Expert	0.75
-# Master	1
-
-# from a quality string and from a number
-
 class AlchemyFactory:
-	def __init__(self, alembic_level='novice', calcinator_level='novice',pestlemortar_level='novice', retort_level='novice', luck_level=20, alchemy_level=1):
+	def __init__(self, alembic_level='novice', calcinator_level='novice',pestlemortar_level='novice', retort_level='novice', luck_level=30, alchemy_level=1):
 		print('- Oblivion Alchemy in Python -')
-		self.al = 1
-		self.ingredients = construct_all_ingredients_df('./processed_flower_effects.csv')
+		# self.al = 1
+		# self.ingredients = construct_df('./processed_flower_effects.csv')
+		self.base_mag_df = construct_df('./effect_base_mag.csv')
 		#lists
 		self.duration_only_effects_list = []
 		self.magnitude_only_effects_list = []
@@ -35,63 +24,115 @@ class AlchemyFactory:
 		self.positive_effects_list = []
 		#constants- may need something else for this to refresh state on things
 		self.effective_alchemy_level = alchemy_level+0.4*(luck_level-50)
+		#self.base_mag = ((self.effective_alchemy_level() + self.gen_ins_fac()*25)/(Effect_Base_Cost/10 * 4)) ** (1/2.28)
+
+	def find_common_effects_between_plants(self,df):
+		effects = df[['Effect 1', 'Effect 2', 'Effect 3', 'Effect 4']].values.flatten()
+		effect_counts = pd.Series(effects).value_counts()
+		common_effects = effect_counts[effect_counts >= 2].index.tolist()
+		return set(common_effects)
+
+	def ingest_plants(self,plants:PlantFactory):
+		pass
+
+	def retrieve_base_mag_for_effect(self,effect):
+		pass
 
 	def get_unique_effects(self):
-		# Getting all column names except 'Flower Name'
-		effect_columns = [col for col in df.columns if col != 'Flower Name']
-		for col in ing_df.columns:
-			ing_df[col] = ing_df[col].apply(lambda x: str(x).lower().replace(" ", "_"))
-
+		ing_df = self.ingredients
+		effect_columns = [col for col in ing_df.columns if col != 'Flower Name']
 		unique_effects = set()
 		for col in effect_columns:
-			unique_effects.update(df[col].dropna().unique())
+			unique_effects.update(ing_df[col].dropna().unique())
+		unique_effects = {effect.strip() for effect in unique_effects if isinstance(effect, str)}
 		pprint.pprint(unique_effects)
+		print(type(unique_effects))
 		return list(unique_effects)
+
+	def get_positive_or_negative_effect(self):
+		return 'Nothing Here yet!'
 
 	def get_effects_from_ingredient(self, ingredient_name):
 		pass
 
 	#need these sort of "helper functions" to sort out the particulars to get the numbers right	
-	def gen_fac(self,effect_type):
-		pass
-
-	def calculate_ins_str(self,thing):
-		if thing == "novice":
-			return 0.1
-		elif thing == "apprentice":
+	def gen_ins_fac(self,effect_type,ins):
+		valid_eff = ['normal','duration_only','magnitude_only']
+		valid_ins =  ['calc','ret_dur_fac','ret_mag_fac', 'alem']
+		if effect_type not in valid_eff:
+			raise Exception(f'effect_type must be one of {valid_eff}')
+		if ins not in valid_ins:
+			raise Exception(f"ins argument must be in {valid_ins}")
+		if effect_type == 'normal' and ins == 'calc':
+			return 0.35
+		elif effect_type == 'duration_only' and ins == 'calc':
 			return 0.25
-		elif thing == "journeyman":
+		elif effect_type == 'magnitude_only' and ins == 'calc':
+			return 0.3
+		elif effect_type == 'normal' and ins == 'ret_dur_fac':
+			return 1
+		elif effect_type == 'duration_only' and ins == 'ret_dur_fac':
+			return 0.35
+		elif effect_type == 'duration_only' and ins == 'ret_dur_fac':
+			raise Exception('Cannot compute Retort Duration Factor for magnitude_only effect_type')
+		elif effect_type == ('normal' or 'magnitude_only') and ins == 'ret_mag_fac':
 			return 0.5
-		elif thing == "expert":
+		elif effect_type == 'duration_only' and ins == 'ret_mag_fac':
+			raise Exception('Cannot compute Retort Magnitude Factor for duration_only effect_type')
+		elif effect_type == 'normal' and ins == 'alem':
+			return 2
+		elif effect_type == 'duration_only' and ins == 'alem':
+			return 2
+		elif effect_type == 'magnitude_only' and ins == 'alem':
+			#^Always 0 in practice, because the only Magnitude-Only effect is a positive one.
+			#really dont get this...
+			return 0
+
+	def gen_magnitude(self,effect):
+		effect_polarity = self.get_positive_or_negative_effect(effect)
+		if effect_polarity == 'negative':
+			Magnitude = Base_Mag * (1 + Calc_Fac*Calcinator_Strength + Ret_Mag_Fac*Retort_Strength - Alem_Fac*Alembic_Strength)
+
+
+	def get_duration(self):
+		effect_polarity = self.get_positive_or_negative_effect()
+
+	def calculate_instrument_str(self,ins_lvl):
+		if ins_lvl == "novice":
+			return 0.1
+		elif ins_lvl == "apprentice":
+			return 0.25
+		elif ins_lvl == "journeyman":
+			return 0.5
+		elif ins_lvl == "expert":
 			return 0.75
-		elif thing == "master":
+		elif ins_lvl == "master":
 			return 1
 		else:
 			raise Exception(f"Incorrect Insturment Level Given you asked for {thing} must be one of the class instance arguments")
 
-	def calculate_strength(self):
-		if alchemy_level == "novice":
+	def calculate_alch_strength(alchemy_level):
+		if 1 <= alchemy_level <= 24:
 			return 0.1
-		elif alchemy_level == "apprentice":
+		elif 25 <= alchemy_level <= 49:
 			return 0.25
-		elif alchemy_level == "journeyman":
+		elif 50 <= alchemy_level <= 74:
 			return 0.5
-		elif alchemy_level == "expert":
+		elif 75 <= alchemy_level <= 99:
 			return 0.75
-		elif alchemy_level == "master":
+		elif alchemy_level == 100:
 			return 1
 		else:
-			raise Exception(f"Incorrect Alchemy Level Given you asked for {thing} must be one of the class instance arguments")
+			raise Exception(f"Incorrect Alchemy Level Given: {alchemy_level}. Must be a number between 1 and 100.")
 
 
-	def calculate_potion_duration_magnitude(self, ing_list:list, Base_Mag=1,Base_Dur=1):
-		Base_Mag = ((self.effective_alchemy_level() + self.calculate_strength*25)/(Effect_Base_Cost/10 * 4)) ** (1/2.28)
-		Base_Dur = 4 * Base_Mag
+	def calc_base_mag_for_effect(self,eff_name):
+		base_mag = ((self.effective_alchemy_level() + self.gen_ins_fac()*25)/(self.get/10 * 4)) ** (1/2.28)
+		return base_mag
 
-		# need to set Alem_Fac, Ret_Mag_Fac, Ret_Dur_Fac, Calc_Fac for one of three cases:
+	def get_magicka_cost(self):
+		return self.effective_alchemy_level + self.calculate_ins_str('pestlemortar_level')*25 
 
-		# for i in ing_list:
-		# 	if 
 
 		Magnitude = Base_Mag * (1 + Calc_Fac*Calcinator_Strength + Ret_Mag_Fac*Retort_Strength- Alem_Fac*Alembic_Strength)
 		Duration = Base_Dur * (1 + Calc_Fac*Calcinator_Strength + Ret_Dur_Fac*Retort_Strength- Alem_Fac*Alembic_Strength)
@@ -107,5 +148,7 @@ class AlchemyFactory:
 
 
 #x = AlchemyFactory().get_effects_from_ingredients(['Sweetcake','Apple'])
-x = AlchemyFactory()
-get_unique_effects()
+alch_factory = AlchemyFactory()
+plant_factory = PlantFactory()
+plant_df = plant_factory.get_plants('corn','carrot','mandrake_root')
+print(alch_factory.find_common_effects_between_plants(plant_df))
